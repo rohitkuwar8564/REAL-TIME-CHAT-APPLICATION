d4668f0b137e6f0667aa198bb32b91dfaed2975c
Ankit Pathak <pathakankit688@gmail.com>
2024-09-06 23:25:59 +0530

    update 4

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import createTokenAndSaveCookie from "../jwt/generateToken.js";
 export const signup = async (req, res) => {
-  const { fullname, email, password, confirmPassword } = req.body;
+  const { name, email, password, confirmPassword } = req.body;
   try {
     if (password !== confirmPassword) {
       return res.status(400).json({ error: "Passwords do not match" });
