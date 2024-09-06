3f011a386af722dbd4f6fc97d529d4a903107f5d
Ankit Pathak <pathakankit688@gmail.com>
2024-09-06 23:05:33 +0530

    Final App Complete

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://chatapp-dh57.onrender.com",
     methods: ["GET", "POST"],
   },
 });
